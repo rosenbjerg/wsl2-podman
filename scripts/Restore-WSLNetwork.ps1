fd9b26d008223d4a1110604fc7129ce7b4d6bc02
Jules <jules+dev@simplelogin.com>
2022-09-08 11:39:21 -0400

    refactor: update README and remove Restore-WSLNetwork.ps1

--- scripts/Restore-WSLNetwork.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-  Restores connectivity for WSL in the event that it is disrupted by the AnyConnect client.
-#>
-
-Get-NetAdapter `
-| Where-Object InterfaceDescription -Like "Cisco AnyConnect" `
-| Set-NetIPInterface -ErrorAction SilentlyContinue -InterfaceMetric 6000 `
-| Out-Null
